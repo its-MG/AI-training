7bd28249bd403473cb5f5e033006232f81cdea03
its-MG <128987407+its-MG@users.noreply.github.com>
2024-03-05 22:42:23 +0100

    initial commit

---
+++ hm1.py
@@ -0,0 +1,14 @@
+import random
+
+n = random.randint(0, 99)
+print("Guess the number between 0 and 99 !")
+
+for i in range(10):
+    nbr = int(input())
+
+    if n == nbr:
+        print("GG u got it!")
+    elif n > nbr:
+        print("Higher")
+    else:
+        print("Lower")
